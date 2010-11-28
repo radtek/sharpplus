217662ddc9b2c6278251b81370f86070b3538182
hubdog@gmail.com <hubdog@gmail.com@db62b038-5a8a-11de-9a1b-6103e4980ab8>
2010-11-28 03:15:06 +0000

    remove vf_logo.c

--- trunk/ffmpeg/libavfilter/allfilters.c
+++ trunk/ffmpeg/libavfilter/allfilters.c
@@ -53,7 +53,6 @@ void avfilter_register_all(void)
     REGISTER_FILTER (FREI0R,      frei0r,      vf);
 	REGISTER_FILTER (FPS,         fps,         vf);
     REGISTER_FILTER (HFLIP,       hflip,       vf);
-	REGISTER_FILTER(LOGO,logo,vf);
     REGISTER_FILTER (NEGATE,      negate,      vf);
     REGISTER_FILTER (NOFORMAT,    noformat,    vf);
     REGISTER_FILTER (NULL,        null,        vf);
