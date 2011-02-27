348e1fdb149bf86cad3109f1dab22050b647704c
hubdog@gmail.com <hubdog@gmail.com@db62b038-5a8a-11de-9a1b-6103e4980ab8>
2011-02-27 04:01:52 +0000

    Change To Use ElementParser instead of Happle

--- trunk/PriceMonitor/KakakuXMLResponse.m
+++ trunk/PriceMonitor/KakakuXMLResponse.m
@@ -1,5 +1,6 @@
 #import "KakakuXMLResponse.h"
 #import "SearchResult.h"
+#import "Utils.h"
 
 @implementation KakakuXMLResponse
 
@@ -12,23 +13,28 @@
 	// Parse the XML document.
 	NSError* error;
 	
-	//TFHpple *parser = [[TFHpple alloc] initWithHTMLData:htmlData];
-	TFHpple *parser = [[TFHpple alloc] initWithHTMLData:data];
+	//NSString * s=@"<html><a href='http://aa'><strong>a</strong>bc</a></html>";
+	//NSData* testData = [Utils stringToData:s];
+	NSString* htmlStr=[Utils cstringToString:data];
+	//NSString* htmlStr=[Utils bytesToString:data];
+	
+	ElementParser* parser = [[[ElementParser alloc] init] autorelease];
+	DocumentRoot* root = [parser parseHTML: htmlStr];
 	//use xpath to query result title
-	NSArray *elements = [parser search:@"//a[@class='title']"];
+	NSArray *elements = [root selectElements:@"a.title"];
+	
     
     totalObjectsAvailableOnServer = [elements count];
     // Now construct our domain-specific object.
     for (NSUInteger i = 0; i < totalObjectsAvailableOnServer; i++) {
         SearchResult *result = [[[SearchResult alloc] init] autorelease];
-        result.title = [[elements objectAtIndex:i] content];
+        result.title = [[elements objectAtIndex:i] contentsText];
 //        result.bigImageURL = [[bigImageURLs objectAtIndex:i] stringValue];
 //        result.thumbnailURL = [[thumbnailURLs objectAtIndex:i] stringValue];
 //        result.bigImageSize = CGSizeMake([[[bigImageWidths objectAtIndex:i] stringValue] intValue], 
 //                                         [[[bigImageHeights objectAtIndex:i] stringValue] intValue]);
         [self.objects addObject:result];
     }
-	[parser release];
     return nil;
 }
 
