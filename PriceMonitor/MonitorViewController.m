ca9c578d2b29dc10c601a03ae5353731278e441c
hubdog <hubdog@gmail.com>
2011-03-20 07:33:56 +0000

    Add Monitor List View

--- PriceMonitor/MonitorViewController.m
+++ PriceMonitor/MonitorViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "MonitorViewController.h"
+#import "MonitorList.h"
+#import "MonitorItem.h"
 
 
 @implementation MonitorViewController
@@ -22,19 +24,33 @@
     [[[UIBarButtonItem alloc] initWithTitle:@"Edit" style:UIBarButtonItemStyleBordered
 									 target:@"tt://monitorEdit?action=new&item=123"
 									 action:@selector(openURLFromButton:)] autorelease];
+	MonitorList* list = [MonitorList monitorList];
+	NSMutableArray* items = [[NSMutableArray alloc] initWithCapacity:100];
+	for (NSInteger i=0; i< [list.itemArray count]; i++) {
+		NSMutableArray* details = [[NSMutableArray alloc] initWithCapacity:100];
+		NSMutableArray* srcDetails = [list.itemArray objectAtIndex:i];
+		for (NSInteger j=0; j<[srcDetails count]; j++) {
+			MonitorItem* item = [srcDetails objectAtIndex:j];
+			TTTableTextItem* tableItem = [TTTableTextItem itemWithText:item.name URL:@""];
+			[details addObject:tableItem];
+		}
+		[items addObject:details];
+	}
 	
-    self.dataSource = [TTSectionedDataSource dataSourceWithObjects:
-					   @"Computer",
-					   [TTTableTextItem itemWithText:@"Mac" URL:@"tt://monitorEdit?action=new&item=123"],
-					   [TTTableTextItem itemWithText:@"Thinkpad" URL:@"tt://monitorEdit?action=new&item=123"],
-					   [TTTableTextItem itemWithText:@"Dell" URL:@"tt://monitorEdit?action=new&item=123"],
-					   @"Game",
-					   [TTTableTextItem itemWithText:@"PS3" URL:@"tt://monitorEdit?action=new&item=123"],
-					   [TTTableTextItem itemWithText:@"Wii" URL:@"tt://monitorEdit?action=new&item=123"],
-					   @"Other",
-					   [TTTableTextItem itemWithText:@"Cannon" URL:@"tt://monitorEdit?action=new&item=123"],
-					   [TTTableTextItem itemWithText:@"Philip" URL:@"tt://monitorEdit?action=new&item=123"],
-					   nil];
+	self.dataSource = [TTSectionedDataSource dataSourceWithItems:items sections:list.sectionArray];
+	
+//    self.dataSource = [TTSectionedDataSource dataSourceWithObjects:
+//					   @"Computer",
+//					   [TTTableTextItem itemWithText:@"Mac" URL:@"tt://monitorEdit?action=new&item=123"],
+//					   [TTTableTextItem itemWithText:@"Thinkpad" URL:@"tt://monitorEdit?action=new&item=123"],
+//					   [TTTableTextItem itemWithText:@"Dell" URL:@"tt://monitorEdit?action=new&item=123"],
+//					   @"Game",
+//					   [TTTableTextItem itemWithText:@"PS3" URL:@"tt://monitorEdit?action=new&item=123"],
+//					   [TTTableTextItem itemWithText:@"Wii" URL:@"tt://monitorEdit?action=new&item=123"],
+//					   @"Other",
+//					   [TTTableTextItem itemWithText:@"Cannon" URL:@"tt://monitorEdit?action=new&item=123"],
+//					   [TTTableTextItem itemWithText:@"Philip" URL:@"tt://monitorEdit?action=new&item=123"],
+//					   nil];
 	
 }
 
