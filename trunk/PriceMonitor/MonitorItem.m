2e4bd27f578a70cd9541bc5d09c256cbf8c290d5
hubdog@gmail.com <hubdog@gmail.com@db62b038-5a8a-11de-9a1b-6103e4980ab8>
2011-03-21 04:52:44 +0000

    Read MonitorItem Information From DB

--- trunk/PriceMonitor/MonitorItem.m
+++ trunk/PriceMonitor/MonitorItem.m
@@ -7,11 +7,39 @@
 //
 
 #import "MonitorItem.h"
+#import "PersistenceManager.h"
 
 
 @implementation MonitorItem
 
 @synthesize monitorId, itemId,price, name, area, category, condition, time, timeType;
+
+-(id) initWithItemId:(NSString*)_itemId{
+	[super init];
+	PersistenceManager * mgr = [PersistenceManager mgr];
+	FMResultSet* rs = [mgr.database executeQuery:[NSString stringWithFormat:
+						@"select * from MonitorList where itemId='%@'", 
+						_itemId
+					]];
+	if (![rs next]){
+		NSLog([mgr.database lastErrorMessage]);
+		return nil;
+	}
+
+	self.monitorId = [rs intForColumn:@"MonitorId"];
+	self.itemId = [rs stringForColumn:@"ItemId"];
+	self.name = [rs stringForColumn:@"Name"];
+	self.price = [rs intForColumn:@"Price"];
+	NSString* category = [rs stringForColumn:@"Category"];
+	self.category = category;
+	self.area = [rs stringForColumn:@"Area"];
+	self.condition = [rs intForColumn:@"Condition"];
+	self.time = [rs intForColumn:@"MonitorTime"];
+	self.timeType = [rs intForColumn:@"TimeType"];
+		
+	[rs close];	
+	return self;
+}
 
 - (void)dealloc
 {
